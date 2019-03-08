66521a26977b7a306e811c74dc9c1f320988d8ac
Sushant Sahu <sushant87@gmail.com>
2019-03-08 18:29:01 +0530

    react demo project running on port 3000

---
+++ react-demo/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Web page running on test.sushantsahu.com
+        </header>
+
+        <p>
+          WIP: This site is owned by sushant sahu         
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
